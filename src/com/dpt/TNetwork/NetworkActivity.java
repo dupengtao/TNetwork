250a1c19732dee9a6214ab88e5264622ae9d9b3a
dupengtao <dupengtao>
2014-06-19 19:51:41 +0800

    add doc

--- src/com/dpt/TNetwork/NetworkActivity.java
+++ src/com/dpt/TNetwork/NetworkActivity.java
@@ -50,6 +50,9 @@ public class NetworkActivity extends Activity {
         onlyLoadInCache();
     }
 
+    /**
+     * load in cache
+     */
     private void onlyLoadInCache() {
         String s = NetClient.loadInCache(UriHelper.getWalletHomeUri());
         if(TextUtils.isEmpty(s)){
